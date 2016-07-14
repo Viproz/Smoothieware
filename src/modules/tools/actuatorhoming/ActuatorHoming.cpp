af2d649f663115581e97904821e81950751b14da
Viproz <viproz12@gmail.fr>
2016-07-15 00:30:27 +0200

    More debugging

--- src/modules/tools/actuatorhoming/ActuatorHoming.cpp
+++ src/modules/tools/actuatorhoming/ActuatorHoming.cpp
@@ -222,6 +222,7 @@ void ActuatorHoming::home(char axes_to_move, Gcode* gcode)
         for ( int c = X_AXIS; c <= Z_AXIS; c++ ) {
             if ( ( axes_to_move >> c) & 1 ) {
 gcode->stream->printf("Moving %d\r\n", c);
+gcode->stream->printf("Debug %d, pins %d %d %d %d %d %d\r\n", 3*this->home_direction[c], this->pins[0].get(), this->pins[1].get(), this->pins[2].get(), this->pins[3].get(), this->pins[4].get(), this->pins[5].get());
                 if(this->pins[c + 3*this->home_direction[c]].get()) {
                     movedAxis = true;
                     STEPPER[c]->manual_step(this->home_direction[c]);
